23f1fdede8cd310d053d3dee60c7e33e47299d4f
Hack Reactor Student <student@hackreactor.com>
2017-07-10 16:40:15 -0700

    Progressing on a few tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,45 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
+  initialize: function(song) {
+    // this.collection.on('change', this.play, this);
+    // initialize new songQ to 0 songs
+    // if (this.models.length === 0 && song === true) {
+    //   this.push(song);
+    // }
+    this.on('add', function(song) {
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+      this.models.push(song);
+    });
+
+    this.on('ended', function() {
+      this.models.shift();
+      this.playFirst();
+    });
+
+    this.on('enqueue', function () {
+      this.models.push();
+    });
+
+    this.on('dequeue', function() {
+      this.models.shift();
+    });
+
+  },
+
+  // add: function(song) {
+  //   console.log(song);
+  //   this.models.push(song);
+    
+  //   // debugger;
+  //   if (this.models.length === 1 && song.length < 2) {
+  //     this.playFirst();
+  //   }
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
